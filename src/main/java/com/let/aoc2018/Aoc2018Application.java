c1d669017cc0869cf80c580b17b294268955ac85
Muller <leticia.muller@sap.com>
2018-12-13 15:14:52 -0200

    First commit

---
+++ src/main/java/com/let/aoc2018/Aoc2018Application.java
@@ -0,0 +1,14 @@
+package com.let.aoc2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aoc2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aoc2018Application.class, args);
+	}
+
+}
+
